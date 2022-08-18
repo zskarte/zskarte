894446ea1ca12d3868c05e0ff029964a8b95ab8a
Yannick Feller <yannick.feller@hotmail.com>
2022-08-18 09:40:57 +0200

    Fixed tests + prettify

--- src/app/shortcut-dialog/shortcut-dialog.component.spec.ts
+++ src/app/shortcut-dialog/shortcut-dialog.component.spec.ts
@@ -1,35 +1,44 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
 
-import { DrawingDialogComponent } from './drawing-dialog.component';
+import { ShortcutDialogComponent } from './shortcut-dialog.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Nl2BrPipeModule } from 'nl2br-pipe';
 
-describe('DrawingDialogComponent', () => {
-  let component: DrawingDialogComponent;
-  let fixture: ComponentFixture<DrawingDialogComponent>;
+describe('ShortcutDialogComponent', () => {
+  let component: ShortcutDialogComponent;
+  let fixture: ComponentFixture<ShortcutDialogComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [MatDialogModule, HttpClientTestingModule],
-      declarations: [DrawingDialogComponent],
+      imports: [MatDialogModule],
+      declarations: [ShortcutDialogComponent],
       providers: [
         {
           provide: MatDialogRef,
           useValue: jasmine.createSpyObj('MatDialogRef', ['close']),
         },
-        {
-          provide: NgxIndexedDBService,
-          useValue: jasmine.createSpyObj('NgxIndexedDBService', ['add']),
-        },
+        { provide: MAT_DIALOG_DATA, useValue: { handlers: [] } },
       ],
       schemas: [NO_ERRORS_SCHEMA],
     });
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DrawingDialogComponent);
+    fixture = TestBed.createComponent(ShortcutDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
