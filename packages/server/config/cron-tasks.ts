4f11fe708df49d580d14c6bc920c0d57bbef76d9
swerder <1330346+swerder@users.noreply.github.com>
2025-01-20 08:50:22 +0100

    chore(cron): fix cron timing, streamline calls

--- packages/server/config/cron-tasks.ts
+++ packages/server/config/cron-tasks.ts
@@ -4,13 +4,14 @@ import { Strapi } from '@strapi/strapi';
 
 export default {
   // Every hour
-  '0 * * * *': async ({ strapi }: { strapi: Strapi }) => await archiveOperations(strapi),
+  '0 * * * *': async ({ strapi }: { strapi: Strapi }) => {
+    await archiveOperations(strapi);
+    await deleteExpiredAccessTokens(strapi);
+  },
   // Every fifteen seconds
   '*/15 * * * * *': async ({ strapi }: { strapi: Strapi }) => await persistMapStates(strapi),
   // Every midnight delete the guest operations
   '0 0 * * *': async ({ strapi }: { strapi: Strapi }) => await deleteGuestOperations(strapi),
-  // Every hour delete expired access tokens
-  '* * * * * *': async ({ strapi }: { strapi: Strapi }) => await deleteExpiredAccessTokens(strapi),
   // Every 5 minutes
-  '*/5 * * * *': ({ strapi }: { strapi: Strapi }) => createMapStateSnapshots(strapi),
+  '*/5 * * * *': async ({ strapi }: { strapi: Strapi }) => await createMapStateSnapshots(strapi),
 };
