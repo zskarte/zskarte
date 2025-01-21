4ee0a76b7988123dcb4bc877e42fb855e90f7237
Yannik Ache Eicher <me@ache.dev>
2025-01-21 10:52:31 +0100

    fix(format): fixed formatting

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   semi: true,
   trailingComma: 'all',
   singleQuote: true,
-  printWidth: 140,
+  printWidth: 120,
   endOfLine: 'auto',
   tabWidth: 2,
   experimentalTernaries: true
