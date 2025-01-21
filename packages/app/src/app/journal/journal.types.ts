68ea7abf388cb6b975dee31f22d15fc7e26c737c
Tim Ogi <timogi@mbp-von-tim-2.home>
2025-01-21 10:57:03 +0100

    added preliminary tpye for journal

---
+++ packages/app/src/app/journal/journal.types.ts
@@ -0,0 +1,36 @@
+export interface JournalEntry {
+  communication_details: string;
+  communication_type: string;
+  createdAt: Date;
+  createdBy?: string;
+  creator: string;
+  date_created: Date;
+  date_visum_decision_deliverer: Date;
+  date_visum_decision_receiver: Date;
+  date_visum_message: Date;
+  date_visum_triage: Date;
+  decision: string;
+  department: string;
+  is_key_message: boolean;
+  message_content: string;
+  message_number: number;
+  message_subject: string;
+  operation?: {
+    id: number;
+    [key: string]: any;
+  };
+  organization?: {
+    id: number;
+    [key: string]: any;
+  };
+  publishedAt: Date;
+  related_symbols: any;
+  sender: string;
+  status: 'awaiting_triage' | 'awaiting_drawing' | 'awaiting_decision' | 'completed';
+  updatedAt: Date;
+  updatedBy?: string;
+  visum_decision_deliverer: string;
+  visum_decision_receiver: string;
+  visum_message: string;
+  visum_triage: string;
+}
