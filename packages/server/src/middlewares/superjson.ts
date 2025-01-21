7da29bfe06e95ee670b041231e522cb3a231b6bb
switzerchees <patrickmichel96@yahoo.de>
2025-01-21 21:32:13 +0100

    fix(middleware): enhance date handling in response serialization

--- packages/server/src/middlewares/superjson.ts
+++ packages/server/src/middlewares/superjson.ts
@@ -3,11 +3,33 @@ import { Context } from 'koa';
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 export const superjson = require('fix-esm').require('superjson') as typeof import('superjson');
 
+function replaceDateValues(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map(replaceDateValues);
+  } else if (obj !== null && typeof obj === 'object') {
+    return Object.keys(obj).reduce((acc, key) => {
+      if (obj[key] && typeof obj[key] === 'object') {
+        acc[key] = replaceDateValues(obj[key]);
+      } else {
+        const keyLower = key.toLowerCase();
+        if (['date', 'createdat', 'updatedat', 'timestamp'].some((k) => keyLower.includes(k))) {
+          acc[key] = new Date(obj[key]);
+        } else {
+          acc[key] = obj[key];
+        }
+      }
+      return acc;
+    }, {} as any);
+  }
+  return obj;
+}
+
 export default (_index, { strapi }: { strapi: Strapi }) => {
   return async (ctx: Context, next) => {
     await next();
     if (ctx.response.body && typeof ctx.response.body === 'object') {
-      const body = superjson.serialize(ctx.response.body);
+      let body = replaceDateValues(ctx.response.body);
+      body = superjson.serialize(body);
       ctx.response.body = body;
     }
   };
