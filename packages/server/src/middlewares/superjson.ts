02c79763a2f23a6c2fa9e08e9dc7a975b981e748
switzerchees <patrickmichel96@yahoo.de>
2025-01-22 08:02:07 +0100

    integrate superjson in frontend api service

--- packages/server/src/middlewares/superjson.ts
+++ packages/server/src/middlewares/superjson.ts
@@ -3,32 +3,24 @@ import { Context } from 'koa';
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 export const superjson = require('fix-esm').require('superjson') as typeof import('superjson');
 
-function replaceDateValues(obj: any): any {
-  if (Array.isArray(obj)) {
-    return obj.map(replaceDateValues);
-  } else if (obj !== null && typeof obj === 'object') {
-    return Object.keys(obj).reduce((acc, key) => {
-      if (obj[key] && typeof obj[key] === 'object') {
-        acc[key] = replaceDateValues(obj[key]);
-      } else {
-        const keyLower = key.toLowerCase();
-        if (['date', 'createdat', 'updatedat', 'timestamp'].some((k) => keyLower.includes(k))) {
-          acc[key] = new Date(obj[key]);
-        } else {
-          acc[key] = obj[key];
-        }
-      }
-      return acc;
-    }, {} as any);
+const transformDates = (data) => {
+  if (!data || typeof data !== 'object') return data;
+
+  for (const key in data) {
+    if (typeof data[key] === 'string' && data[key].match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)) {
+      data[key] = new Date(data[key]);
+    } else if (typeof data[key] === 'object' && data[key] !== null) {
+      transformDates(data[key]);
+    }
   }
-  return obj;
-}
+  return data;
+};
 
 export default (_index, { strapi }: { strapi: Strapi }) => {
   return async (ctx: Context, next) => {
     await next();
     if (ctx.response.body && typeof ctx.response.body === 'object') {
-      let body = replaceDateValues(ctx.response.body);
+      let body = transformDates(ctx.response.body);
       body = superjson.serialize(body);
       ctx.response.body = body;
     }
