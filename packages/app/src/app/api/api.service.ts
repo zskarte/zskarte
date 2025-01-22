fdde37f625eddf5e94b9bbd9c0f7428b884c9773
switzerchees <patrickmichel96@yahoo.de>
2025-01-22 10:17:44 +0100

    feat(api): fix api.service for login

--- packages/app/src/app/api/api.service.ts
+++ packages/app/src/app/api/api.service.ts
@@ -1,9 +1,10 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { deserialize } from 'superjson';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { lastValueFrom, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { SessionService } from '../session/session.service';
 import transformResponse, { TransformerOptions } from './transformer';
+import { deserialize, SuperJSONResult } from 'superjson';
 
 export interface IApiRequestOptions {
   headers?: { [key: string]: string };
@@ -22,6 +23,8 @@ export interface ApiResponse<T> {
   providedIn: 'root',
 })
 export class ApiService {
+  private _http = inject(HttpClient);
+
   private _apiUrl = environment.apiUrl;
   private _session!: SessionService;
 
@@ -33,13 +36,17 @@ export class ApiService {
     return this._apiUrl;
   }
 
-  private async _retry<RESPONSE>(fn: Promise<RESPONSE>, options?: IApiRequestOptions): Promise<ApiResponse<RESPONSE>> {
+  private async _retry<RESPONSE>(
+    fn: Observable<RESPONSE>,
+    options?: IApiRequestOptions,
+  ): Promise<ApiResponse<RESPONSE>> {
     const maxRetries = options?.retries || 3;
     let lastError: HttpErrorResponse = new HttpErrorResponse({ status: 0 });
     for (let i = 0; i < maxRetries; i++) {
       try {
+        const rawResponse = await lastValueFrom(fn);
         return {
-          result: transformResponse(await fn),
+          result: transformResponse(deserialize(rawResponse as SuperJSONResult)),
         };
       } catch (error) {
         if (error instanceof HttpErrorResponse) {
@@ -51,28 +58,44 @@ export class ApiService {
     return { error: lastError };
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async post<RESPONSE = any, REQUEST = any>(
-    path: string,
-    body: REQUEST,
+    subUrl: string,
+    params: REQUEST,
     options?: IApiRequestOptions,
   ): Promise<ApiResponse<RESPONSE>> {
-    return await this._retry(this._fetch({ method: 'POST', path, body, options }));
+    return await this._retry(
+      this._http.post<RESPONSE>(`${this._apiUrl}${subUrl}`, params, { headers: this._getDefaultHeaders(options) }),
+    );
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async put<RESPONSE = any, REQUEST = any>(
-    path: string,
-    body: REQUEST,
+    subUrl: string,
+    params: REQUEST,
     options?: IApiRequestOptions,
   ): Promise<ApiResponse<RESPONSE>> {
-    return await this._retry(this._fetch({ method: 'PUT', path, body, options }));
+    return await this._retry(
+      this._http.put<RESPONSE>(`${this._apiUrl}${subUrl}`, params, { headers: this._getDefaultHeaders(options) }),
+    );
   }
 
-  public async get<RESPONSE = any>(path: string, options?: IApiRequestOptions): Promise<ApiResponse<RESPONSE>> {
-    return await this._retry(this._fetch({ method: 'GET', path, options }));
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public get$<RESPONSE = any>(subUrl: string, options?: IApiRequestOptions): Observable<RESPONSE> {
+    return new Observable((subscriber) => {
+      this._http.get<RESPONSE>(`${this._apiUrl}${subUrl}`, { headers: this._getDefaultHeaders(options) }).subscribe({
+        next: (response) => subscriber.next(deserialize(response as SuperJSONResult)),
+        error: (error) => subscriber.error(error),
+        complete: () => subscriber.complete(),
+      });
+    });
   }
 
-  public async delete<RESPONSE = any>(path: string, options?: IApiRequestOptions): Promise<ApiResponse<RESPONSE>> {
-    return await this._retry(this._fetch({ method: 'DELETE', path, options }));
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public async get<RESPONSE = any>(subUrl: string, options?: IApiRequestOptions): Promise<ApiResponse<RESPONSE>> {
+    return await this._retry(
+      this._http.get<RESPONSE>(`${this._apiUrl}${subUrl}`, { headers: this._getDefaultHeaders(options) }),
+    );
   }
 
   private _getDefaultHeaders(options?: IApiRequestOptions): { [key: string]: string } {
@@ -85,33 +108,10 @@ export class ApiService {
     return { ...defaults, ...(options?.headers || {}) };
   }
 
-  private async _fetch({
-    path,
-    method,
-    body,
-    options,
-  }: {
-    path: string;
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE';
-    body?: any;
-    options?: IApiRequestOptions;
-  }): Promise<any> {
-    const headers = this._getDefaultHeaders(options);
-    const response = await fetch(`${this._apiUrl}${path}`, {
-      method,
-      headers,
-      body: JSON.stringify(body),
-    });
-
-    if (!response.ok) {
-      console.error('Error on request', { method, path, body, options });
-      throw new Error('Error on request');
-    }
-
-    if (response.status === 204) {
-      return undefined;
-    }
-
-    return deserialize(await response.json());
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public async delete<RESPONSE = any>(subUrl: string, options?: IApiRequestOptions): Promise<ApiResponse<RESPONSE>> {
+    return await this._retry(
+      this._http.delete<RESPONSE>(`${this._apiUrl}${subUrl}`, { headers: this._getDefaultHeaders(options) }),
+    );
   }
 }
