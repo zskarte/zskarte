c000d726ea2ec6b4a55b6d3b52e6bec2d2c670c6
Yannik Ache Eicher <me@ache.dev>
2025-01-24 14:48:12 +0100

    fix(map-renderer): translations

--- packages/app/src/app/map-renderer/map-renderer.component.ts
+++ packages/app/src/app/map-renderer/map-renderer.component.ts
@@ -176,7 +176,7 @@ export class MapRendererComponent implements AfterViewInit {
 
       if (remove) {
         const confirmation = this._dialog.open(ConfirmationDialogComponent, {
-          data: this.i18n.get('deletionNotification'),
+          data: this.i18n.get('removeFeatureFromMapConfirm'),
         });
         const result = await lastValueFrom(confirmation.afterClosed());
         if (result) {
