f1c979f85119ba89566396ccd8b7afd2c8e27a2c
lucamuh <lucamuh@besonet.ch>
2025-01-24 08:15:59 +0100

    default values for iconsOffset

--- packages/types/sign/interfaces.ts
+++ packages/types/sign/interfaces.ts
@@ -168,6 +168,13 @@ export function defineDefaultValuesForSignature(signature: Sign) {
     signature.fillStyle.size ?? signatureDefaultValues.fillStyleSize;
   signature.fillStyle.spacing =
     signature.fillStyle.spacing ?? signatureDefaultValues.fillStyleSpacing;
+  signature.iconsOffset = signature.iconsOffset ?? signatureDefaultValues.iconsOffset;
+  signature.iconsOffset.x = signature.iconsOffset.x ?? signatureDefaultValues.iconsOffset.x;
+  signature.iconsOffset.y = signature.iconsOffset.y ?? signatureDefaultValues.iconsOffset.y;
+  signature.iconsOffset.endHasDifferentOffset =
+    signature.iconsOffset.endHasDifferentOffset ?? signatureDefaultValues.iconsOffset.endHasDifferentOffset;
+  signature.iconsOffset.endX = signature.iconsOffset.endX ?? signatureDefaultValues.iconsOffset.endX;
+  signature.iconsOffset.endY = signature.iconsOffset.endY ?? signatureDefaultValues.iconsOffset.endY;
   signature.protected = signature.protected ?? signatureDefaultValues.protected;
   signature.labelShow = signature.labelShow ?? signatureDefaultValues.labelShow;
   signature.arrow = signature.arrow ?? signatureDefaultValues.arrow;
