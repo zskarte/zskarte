cf676e3d1bc4bd14a806baa81994174abc2129ad
swerder <1330346+swerder@users.noreply.github.com>
2026-01-19 15:29:40 +0100

    feat(import mapLayer): fix admin-auth for ui trigger

--- packages/server/src/middlewares/admin-auth.ts
+++ packages/server/src/middlewares/admin-auth.ts
@@ -1,20 +1,24 @@
+import jwt from 'jsonwebtoken';
+
 module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
     const token = ctx.request.get('Authorization')?.replace('Bearer ', '');
     if (!token) return ctx.unauthorized();
-    
+
     try {
-      const jwtService = strapi.admin.services.token;
-      const decoded = jwtService.decodeJwtToken(token, {
-        subject: strapi.config.get('server.adminUrl') ? 'admin' : undefined
+      const jwtSecret = strapi.config.get('admin.auth.secret') || strapi.config.get('plugin.admin.auth.secret');
+      let decoded = jwt.verify(token, jwtSecret, {
+        subject: strapi.config.get('server.adminUrl') ? 'admin' : undefined,
       });
-      if (decoded.isValid && decoded.payload?.id){
+      if (typeof decoded === 'object' && decoded.id) {
         const userService = strapi.admin.services.user;
-        const userData = await userService.findOne(decoded.payload.id);
-        strapi.log.info(`admin-auth middleware, decoded: ${JSON.stringify(decoded)}, userData: id: ${userData?.id}, firstname: ${userData?.firstname}, lastname: ${userData?.lastname}`);
-        if (userData?.isActive && !userData?.blocked && userData?.roles.some((r) => r.code === 'strapi-super-admin')){
+        const userData = await userService.findOne(decoded.id);
+        strapi.log.info(
+          `admin-auth middleware, decoded: ${JSON.stringify(decoded)}, userData: id: ${userData?.id}, firstname: ${userData?.firstname}, lastname: ${userData?.lastname}`,
+        );
+        if (userData?.isActive && !userData?.blocked && userData?.roles.some((r) => r.code === 'strapi-super-admin')) {
           ctx.state.adminUser = userData;
-          return next();  
+          return next();
         }
       }
     } catch (err) {
